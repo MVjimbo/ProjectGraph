7475fa159149c07f146d47c0888c04707c451af5
MVjimbo <maslo-86@ya.ru>
2020-06-06 11:13:51 +0300

    Added one more method

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def path_from_B_to_H(G,start_nodes_buildings,end_nodes_hodpitals,buildings,hospi
     path_for_building={}
     for node,building in start_nodes_buildings.items():
         is_Achivable=True
-        Dijkstra_result = methods.Dijkstra(G, node, list(end_nodes_hodpitals.keys()))
+        Dijkstra_result = methods.Dijkstra_list_ends_nearest(G, node, list(end_nodes_hodpitals.keys()))
         if Dijkstra_result == False:
             is_Achivable = False
         if is_Achivable and (path_for_building.get(building)==None or path_for_building.get(building)[1]>Dijkstra_result[1]):
@@ -51,6 +51,68 @@ def path_from_B_to_H(G,start_nodes_buildings,end_nodes_hodpitals,buildings,hospi
         print()
 
 
+def path_from_H_to_B(G,start_nodes_hospitals,end_nodes_buildings,buildings,hospitals):
+    path_for_hospitals={}
+    for node,building in start_nodes_hospitals.items():
+        is_Achivable=True
+        Dijkstra_result = methods.Dijkstra_list_ends_nearest(G, node, list(end_nodes_buildings.keys()))
+        if Dijkstra_result == False:
+            is_Achivable = False
+        if is_Achivable and (path_for_hospitals.get(building)==None or path_for_hospitals.get(building)[1]>Dijkstra_result[1]):
+            path_for_hospitals.update({building:Dijkstra_result})
+    for building, arr in path_for_hospitals.items():
+        print(building)
+        print(arr[0])
+        print(arr[1])
+        print()
+
+
+def path_from_B_to_H_back(G,nodes_buildings,nodes_hodpitals,buildings,hospitals):
+    path_from_B_to_H={}
+    for node_b,building in nodes_buildings.items():
+        is_Achivable=True
+        Dijkstra_result_dict=methods.Dijkstra_list_ends(G,node_b,list(nodes_hodpitals.keys()))
+        for node_h,hospital in nodes_hodpitals.items():
+            Dijkstra_result=Dijkstra_result_dict.get(node_h)
+            #Dijkstra_result = methods.Dijkstra_one_end(G, node_b, node_h)
+            # if Dijkstra_result==False:
+            #     is_Achivable=False
+            if path_from_B_to_H.get((building,hospital))==None or path_from_B_to_H.get((building,hospital))[1]>Dijkstra_result[1]:
+                path_from_B_to_H.update({(building,hospital): Dijkstra_result})
+        print("Done hospitals")
+    print("Done for buildings")
+
+    path_from_H_to_B = {}
+    for node_h, hospital in nodes_hodpitals.items():
+        is_Achivable = True
+        Dijkstra_result_dict = methods.Dijkstra_list_ends(G, node_h, list(nodes_buildings.keys()))
+        for node_b, building in nodes_buildings.items():
+            Dijkstra_result=Dijkstra_result_dict.get(node_b)
+            # Dijkstra_result = methods.Dijkstra_one_end(G, node_h, node_b)
+            # if Dijkstra_result == False:
+            #     is_Achivable = False
+            if path_from_H_to_B.get((hospital, building)) == None or path_from_H_to_B.get((hospital,building))[1]>Dijkstra_result[1]:
+                path_from_H_to_B.update({(hospital,building): Dijkstra_result})
+        print("Done buildings")
+    print("Done for hospitals")
+    print(len(path_from_B_to_H))
+    print(len(path_from_H_to_B))
+
+    path_back_and_foth={}
+
+    for building in buildings:
+        for hospital in hospitals:
+            if path_back_and_foth.get((building[0],hospital[0]))==None or path_back_and_foth.get((building[0],hospital[0]))>path_from_B_to_H.get((building[0],hospital[0]))[1]+path_from_H_to_B.get((hospital[0],building[0]))[1]:
+                path_back_and_foth.update({(building[0],hospital[0]):[path_from_B_to_H.get(
+                    (building[0], hospital[0]))[0],path_from_H_to_B.get((hospital[0], building[0]))[0],path_from_B_to_H.get(
+                    (building[0], hospital[0]))[1] + path_from_H_to_B.get((hospital[0], building[0]))[1]]})
+
+    for key,values in path_back_and_foth.items():
+        print(key)
+        print(values[0])
+        print(values[1])
+        print(values[2])
+        print()
 
 
 if __name__=='__main__':
@@ -58,17 +120,17 @@ if __name__=='__main__':
     Gtry=nx.MultiDiGraph()
     Gtry.add_nodes_from([1,2,3,4,5,6])
     Gtry.add_edges_from([(1,2,{"length":100}),(1,3,{"length":1}),(2,4,{"length":100}),(3,4,{"length":1}),(3,5,{"length":100}),(4,6,{"length":50})])
-    path=methods.Dijkstra(Gtry, 1, [5,6])
+    path=methods.Dijkstra_list_ends(Gtry, 1, [5,6])
     print(path)
     #getGraph()   Запусти сначала эту функцию, она сохраняет граф, после этого можно функции ниже запускать
     G=ox.io.load_graphml("C:\Python\ProjectG\data\g.graphml")  #Путь до файла
     #fig, ax = ox.plot_graph(ox.project_graph(G))
-    buildings=random.sample(DB.getBuildings(),100)
-    hospitals=random.sample(DB.getHospitals(),10)
+    buildings=random.sample(DB.getBuildings(),5)
+    hospitals=random.sample(DB.getHospitals(),3)
     nearest_nodes_hospitals=get_nearest_nodes(G,hospitals)
     print(nearest_nodes_hospitals)
     nearest_nodes_buildings=get_nearest_nodes(G,buildings)
-    path_from_B_to_H(G,nearest_nodes_buildings,nearest_nodes_hospitals,buildings,hospitals)
+    path_from_B_to_H_back(G,nearest_nodes_buildings,nearest_nodes_hospitals,buildings,hospitals)
 
 
 
--- methods.py
+++ methods.py
@@ -1,4 +1,4 @@
-def Dijkstra(G,start,end):
+def Dijkstra_list_ends_nearest(G,start,end): #end-Список вершин, Дейкстра возвращает кратчайшее расстояние до близжайшей вершины из end
     len_shortest_way={node: 100000 for node in G.nodes()}
     len_shortest_way.update({start:0})
 
@@ -45,10 +45,122 @@ def Dijkstra(G,start,end):
     shortest_way=[]
     shortest_way.append(reading_node)
 
-    result_node=reading_node
+    end_node=reading_node
 
     while (reading_node!=start):
         reading_node=previoust_node.get(reading_node)
         shortest_way.append(reading_node)
     shortest_way.reverse()
-    return [shortest_way,len_shortest_way.get(result_node)]
+    return [shortest_way,len_shortest_way.get(end_node)]
+
+def Dijkstra_one_end(G,start,end):#end-одна вершина, Дейкстра возвращает кратчайшее расстояние до нее
+    len_shortest_way={node: 100000 for node in G.nodes()}
+    len_shortest_way.update({start:0})
+
+    previoust_node={}
+    previoust_node.update({start:start})
+
+    reading_node=start
+    visited_nodes=[]
+    nodes_to_visit=[start]
+
+    while end!=reading_node and len(nodes_to_visit)!=0:
+        sort_len=[len_shortest_way[node] for node in nodes_to_visit]
+        sort_len.sort()
+
+        for node in nodes_to_visit:
+            if len_shortest_way.get(node)==sort_len[0]:
+                visited_nodes.append(reading_node)
+                reading_node=node
+                nodes_to_visit.remove(reading_node)
+                break
+
+        adjacent_nodes=[key for key in G[reading_node].keys()]
+
+        adjacent_nodes=list(set(adjacent_nodes))
+
+        for node in visited_nodes:
+            if adjacent_nodes.count(node)>0:
+                adjacent_nodes.remove(node)
+
+        for node in adjacent_nodes:
+            nodes_to_visit.append(node)
+
+        nodes_to_visit=list(set(nodes_to_visit))
+
+        for node in adjacent_nodes:
+            if len_shortest_way.get(node)>len_shortest_way.get(reading_node)+G[reading_node][node][0]["length"]:
+                length=len_shortest_way.get(reading_node) + G[reading_node][node][0]["length"]
+                len_shortest_way.update({node:length})
+                previoust_node.update({node:reading_node})
+
+    if (end!=reading_node):
+        return False
+
+    shortest_way=[]
+    shortest_way.append(reading_node)
+
+
+    while (reading_node!=start):
+        reading_node=previoust_node.get(reading_node)
+        shortest_way.append(reading_node)
+    shortest_way.reverse()
+    return [shortest_way,len_shortest_way.get(end)]
+
+
+def Dijkstra_list_ends(G,start,end): #end-Список вершин, Дейкстра возвращает кратчайшее расстояние до близжайшей вершины из end
+    len_shortest_way={node: 100000 for node in G.nodes()}
+    len_shortest_way.update({start:0})
+
+    previoust_node={}
+    previoust_node.update({start:start})
+
+    reading_node=start
+    visited_nodes=[]
+    nodes_to_visit=[start]
+
+    while len(nodes_to_visit)!=0:
+        sort_len=[len_shortest_way[node] for node in nodes_to_visit]
+        sort_len.sort()
+
+        for node in nodes_to_visit:
+            if len_shortest_way.get(node)==sort_len[0]:
+                visited_nodes.append(reading_node)
+                reading_node=node
+                nodes_to_visit.remove(reading_node)
+                break
+
+        adjacent_nodes=[key for key in G[reading_node].keys()]
+
+        adjacent_nodes=list(set(adjacent_nodes))
+
+        for node in visited_nodes:
+            if adjacent_nodes.count(node)>0:
+                adjacent_nodes.remove(node)
+
+        for node in adjacent_nodes:
+            nodes_to_visit.append(node)
+
+        nodes_to_visit=list(set(nodes_to_visit))
+
+        for node in adjacent_nodes:
+            if len_shortest_way.get(node)>len_shortest_way.get(reading_node)+G[reading_node][node][0]["length"]:
+                length=len_shortest_way.get(reading_node) + G[reading_node][node][0]["length"]
+                len_shortest_way.update({node:length})
+                previoust_node.update({node:reading_node})
+
+    end_ways={}
+
+    for node in end:
+        reading_node=node
+        end_node = reading_node
+
+        shortest_way=[]
+        shortest_way.append(reading_node)
+
+        while (reading_node!=start):
+            reading_node=previoust_node.get(reading_node)
+            shortest_way.append(reading_node)
+        shortest_way.reverse()
+        end_ways.update({node:[shortest_way,len_shortest_way.get(end_node)]})
+    return end_ways
